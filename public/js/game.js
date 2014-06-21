cd86257f16d605174f4fa77c7ff66d40b7d0e117
craigmcginley <craigmcginley@gmail.com>
2014-06-20 22:41:33 -0400

    made candies look better, added bg candy

--- public/js/game.js
+++ public/js/game.js
@@ -27,12 +27,13 @@ var spriteSheet = new Image();
 spriteSheet.src = "sprite-sheet.png";
 spriteSheet.addEventListener('load', init, false);
 
-allColors = [["#fcc", "#f69", "#f06", "#903"],
-             ["#fcc", "#a69", "#a06", "#403"],
-             ["#fff", "#ffff66", "ffff00", "#cccc00"],
-             ["#fff", "#99f", "66f", "#0000cc"],
-             ["#fff", "#99ff99", "66ff66", "33cc33"],
-             ["#fff", "#0CC", "099", "#066"]]
+allColors = [["#ffffff", "#ff99ff", "#ff66ff", "#990099"],
+             ["#ffffff", "#cc99ff", "#9966cc", "#330066"],
+             ["#ffffff", "#ffff66", "ffff00", "#cccc00"],
+             ["#ffffff", "#3399ff", "#0066cc", "#003366"],
+             ["#ffffff", "#99ff99", "#66ff66", "#33cc33"],
+             ["#ffffff", "#ff9999", "#ff6666", "#cc0033"],
+             ["#ffffff", "#ffcc99", "#ff9900", "#cc3300"]]
 
 var radialGradient;
 
@@ -59,9 +60,13 @@ drawX2: 2048
 var player;
 
 var candies = [];
+var farCandies = [];
 
 while (candies.length < 15) {
-  candies.push(new Candy(getRandomNum(gameWidth, gameWidth + 400), getRandomNum(0, gameHeight), getRandomNum(10, 50), allColors));
+  candies.push(new Candy(getRandomNum(gameWidth, gameWidth + 400), getRandomNum(0, gameHeight), getRandomNum(15, 75), allColors, 'near'));
+}
+while (farCandies.length < 200) {
+  farCandies.push(new Candy(getRandomNum(0, gameWidth), getRandomNum(0, 470), getRandomNum(.7, 4), allColors, 'far'));
 }
 
 
@@ -81,19 +86,31 @@ function init() {
 
 function loop() {
   if (isPlaying) {
+
     moveBg(bgSky, 1);
     moveBg(bgGround, 8);
-    player.draw();
     clearCtxCandy();
+
+    for (var i = 0; i < farCandies.length; i++) {
+      if (farCandies[i].x < -10) {
+        farCandies.splice(i, 1);
+        farCandies.push(new Candy(getRandomNum(gameWidth+10, gameWidth+20), getRandomNum(0, 470), getRandomNum(.7, 4), allColors, 'far'));
+      }
+      farCandies[i].draw();
+    }
+
+    player.draw();
+
     for (var i = 0; i < candies.length; i++) {
-      if (candies[i].x < -50) {
+      if (candies[i].x < -75) {
         candies.splice(i, 1);
-        candies.push(new Candy(getRandomNum(gameWidth+250, gameWidth + 500), getRandomNum(0, gameHeight), getRandomNum(10, 50), allColors));
+        candies.push(new Candy(getRandomNum(gameWidth+250, gameWidth + 500), getRandomNum(0, gameHeight), getRandomNum(15, 75), allColors, 'near'));
       }
       candies[i].draw();
     }
 
     requestAnimFrame(loop);
+
   }
 }
 
@@ -144,7 +161,7 @@ function Player() {
   this.rightX = this.drawX + this.width;
   this.topY = this.drawY;
   this.bottomY = this.drawY + this.height;
-  this.speed = 2;
+  this.speed = 3;
   this.moveUp = false;
   this.moveDown = false;
   this.moveLeft = false;
@@ -191,14 +208,18 @@ Player.prototype.motion = function() {
 
 
 // Candy Functions
-function Candy(x, y, radius, allColors) {
+function Candy(x, y, radius, allColors, distance) {
   this.x = x;
   this.y = y;
   this.radius = radius;
   // this.hits = radius;
   // this.destroyed = false;
-  this.colors = allColors[Math.floor(getRandomNum(0,5))];
-  this.xVel = (1/radius) * 100;
+  this.colors = allColors[Math.floor(getRandomNum(0,6))];
+  if (distance === 'near') {
+    this.xVel = (1/radius) * 100;
+  } else {
+    this.xVel = radius;
+  }
   // this.containsPoint = function(x, y) {
   //   if (x >= (this.x - this.radius) && x <= (this.x + this.radius)) {
   //     if (y >= (this.y - this.radius) && y <= (this.y + this.radius)) {
@@ -222,7 +243,7 @@ function clearCtxCandy() {
 }
 
 function drawCircle(x, y, radius, colors) {
-  radialGradient = ctxCandy.createRadialGradient(x-8, y-12, 0, x-20, y-20, radius + 10);
+  radialGradient = ctxCandy.createRadialGradient(x-radius+((radius/100) * 70), y-radius+((radius/100) * 70), 0, x-radius+((radius/100) * 70), y-radius+((radius/100) * 70), (radius-(radius/100)*2));
   radialGradient.addColorStop(0, colors[0]);
   radialGradient.addColorStop(0.4, colors[1]);
   radialGradient.addColorStop(0.6, colors[2]);
